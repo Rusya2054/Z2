b9b2072e61f224f3da8f5f747b49fc1a41b31172
Rusya2054 <Rusya2054@mail.ru>
2025-01-18 16:05:39 +0500

    added documentation

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from modules import *
 
 
 if __name__ == '__main__':
-    vertex = get_info_from_file(r'Tire.obj')  # получение точек
-    new_vertex = decard_to_cylinder(vertex, 0.001)  # перевод точек в цилиндрическиую систему координат
-    new_vertex = cylinder_to_decard(new_vertex)  # перевод точек из цилиндрической в декартову систему координат
-    new_vertex = for_Z(vertex, 0.01)  # ремаштабирование оси Z
+    vertex = get_points_from_blender(r'Tire.obj')
 
-    model1 = get_info_from_file(r'rim.obj')  # получение точек
-    # new_vertex=decard_to_cylinder([[-2,-2,1]],1)
-    # print(new_vertex[0])
-    # new_vertex=cylinder_to_decard(new_vertex)
-    # print(new_vertex[0])
-    # new_vertex=for_Z(vertex,0.0001) #ремаштабирование точки
+    # перевод точек в цилиндрическую систему координат
+    new_vertexes = cartesian_to_cylinder(vertex, 0.1)
+    # перевод точек из цилиндрической в декартову систему координат
+    new_vertexes = cylinder_to_cartesian(new_vertexes)
 
-    plotting_3D(new_vertex)
-    # plotting_3D(model1)
-    # plotting_3D(vertex)
-    # plotting_3D(list([[-33.8284, 586.5621, 52.8456]]))
+    new_vertexes = z_rescaling(new_vertexes, 10)  # ремаштабирование оси Z
+
+    # получение точек
+    model1 = get_points_from_blender(r'rim.obj')
+    # new_vertex=cylinder_to_cartesian(new_vertex)
+    # print(new_vertex[0])
+    # new_vertex=z_rescaling(vertex, 0.0001) #ремаштабирование точки
+
+    plot_3d_points([*model1, *new_vertexes])
+    # plot_3d_points(new_vertexes)
+    # plot_3d_points(vertex)
+
+    # plot_3d_points(list([[-33.8284, 586.5621, 52.8456]]))
+    # plot_3d_points([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]])
--- modules.py
+++ modules.py
@@ -1,24 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from typing import List
 
-
-def get_info_from_file(path):
-    vertex, faces = [], []
+def get_points_from_blender(path: str) -> List[List[float]]:
+    """
+    Reads a 3D model file in OBJ format and extracts vertexes coordinates.
+    :param path: A string representing the file path to the OBJ file.
+    :return: A list of lists, where each inner list contains the coordinates (x, y, z) of a vertexes.
+    
+    Returns an empty list if the file is not in OBJ format or if no vertices are found.
+    """
+    vertexes = []
+    if not path.endswith(".obj"):
+        return vertexes
     with open(path) as f:
         for line in f:
             if line.startswith('v '):
-                # vertex.append([float(i) for i in line.split()[1:]] + [1])
-                vertex.append([float(i) for i in line.split()[1:]])
+                # vertexes.append([float(i) for i in line.split()[1:]] + [1])
+                vertexes.append([float(i) for i in line.split()[1:]])
+    return vertexes
 
-    return vertex
 
+def plot_3d_points(vertexes: List[List[float]]) -> None:
+    """
+    Plots a 3D scatter plot of a given list of 3D points.
+        This function takes a list of 3D points, where each point is represented as a list of three
+    floats and creates a 3D scatter plot using matplotlib.
 
-def plotting_3D(array):
+    :param vertexes:  A list of lists, where each inner list contains Cartesian coordinates [x, y, z]
+    :return: None: This function does not return anything. It displays the 3D scatter plot directly.
+
+    """
     figure = plt.figure()
     ax = figure.add_subplot(111, projection='3d')
     x, y, z = [], [], []
-    for i in array:
+    for i in vertexes:
         x.append(i[0])
         y.append(i[1])
         z.append(i[2])
@@ -26,22 +43,58 @@ def plotting_3D(array):
     plt.show()
 
 
-def decard_to_cylinder(array, value):
-    new_arr = []
-    for i in array:
-        new_arr.append([value * np.sqrt(pow(i[0], 2) + pow(i[1], 2)), math.atan2(i[1], i[0]) * (180 / np.pi), i[2]])
-    return new_arr
+def cartesian_to_cylinder(vertexes: List[List[float]], value: float = 1.0) -> List[List[float]]:
+    """
+    Converts Cartesian coordinates (x, y, z) to cylindrical coordinates (r, θ, z).
+
+    :param vertexes: A list of lists, where each inner list contains Cartesian coordinates [x, y, z]
+    :param value: A scaling factor for the radial distance (default is 1.0).
+    :return: A list of lists, where each inner list contains cylindrical coordinates [r, θ, z].
+    angle θ in degrees.
+    """
+    new_vertexes = []
+    """
+    -> 1. Calculate the radial distance (r) using the Euclidean
+        distance formula: r = value * sqrt(x² + y²).
+    -> 2. Calculate the azimuthal angle (θ) using the arctangent function:
+        θ = atan2(y, x) * (180 / π) to convert radians to degrees.
+    -> 3. Keep the z-coordinate unchanged.
+    """
+    for i in vertexes:
+
+        new_vertexes.append([value * np.sqrt(pow(i[0], 2) + pow(i[1], 2)),
+                             math.atan2(i[1], i[0]) * (180 / np.pi), i[2]])
+    return new_vertexes
 
 
-def cylinder_to_decard(array):
-    new_arr = []
-    for i in array:
-        new_arr.append([i[0] * np.cos(i[1] * np.pi / 180), i[0] * np.sin(i[1] * np.pi / 180), i[2]])
-    return new_arr
+def cylinder_to_cartesian(vertexes: List[List[float]])-> List[List[float]]:
+    """
+    Converts cylindrical coordinates (r, θ, z) to Cartesian coordinates (x, y, z).
+
+    :param vertexes: A list of lists, where each inner list contains cylindrical coordinates [r, θ, z]. angel θ in degrees
+    :return:
+    """
+    new_vertexes = []
+    """
+    -> 1. Calculate x = r * cos(θ), where θ is converted from degrees to radians.
+    -> 2. Calculate y = r * sin(θ), where θ is converted from degrees to radians.
+    -> 3. Keep the z-coordinate unchanged.
+    """
+    for i in vertexes:
+        new_vertexes.append([i[0] * np.cos(i[1] * np.pi / 180), i[0] * np.sin(i[1] * np.pi / 180), i[2]])
+    return new_vertexes
 
 
-def for_Z(array, value):
-    new_arr = []
-    for i in array:
-        new_arr.append([i[0], i[1], i[2] * value])
-    return new_arr
+def z_rescaling(vertexes: List[List[float]], value: float = 1.0) -> List[List[float]]:
+    """
+    Rescales the z-coordinate of each vertex by a given scaling factor.
+
+    :param vertexes: A list of lists, where each inner list contains coordinates [x, y, z] or [r, θ, z].
+    :param value: A scaling factor for the z-coordinate (default is 1.0).
+    :return: A list of lists, where each inner list contains the rescaled
+     coordinates [x, y, z * value] or [r, θ, z * value]
+    """
+    new_vertexes = []
+    for i in vertexes:
+        new_vertexes.append([i[0], i[1], i[2] * value])
+    return new_vertexes
